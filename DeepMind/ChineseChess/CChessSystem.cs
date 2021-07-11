88c2957b52e7a5ae76c6817fd8cdac0770c05e5c
日瀬 健人 <kentnewbie@hotmail.com>
2021-07-11 23:50:16 +0900

    U

--- DeepMind/ChineseChess/CChessSystem.cs
+++ DeepMind/ChineseChess/CChessSystem.cs
@@ -29,6 +29,7 @@ namespace DeepMind.ChineseChess
 
         public List<CChessMove> LegalMoveList()
         {
+            
             //搜尋每一個子
             byte player = CurrentBoard.IsBlackTurn ? (byte)1 : (byte)0;
             List<CChessMove> result = new List<CChessMove>();
@@ -65,77 +66,92 @@ namespace DeepMind.ChineseChess
                                 break;
                             case 'B':
                             case 'b':
-                                //四方向 - 打和移動
                                 bool isRun = true;
-                                for (int k = i - 1; k >= 0; k--)
-                                { 
-                                    if (isRun && CurrentBoard[k, j] == ' ')
-                                        result.Add(new CChessMove(CurrentBoard[i, j], i, j, k, j));
+                                bool CheckStraightBao(int x, int y)
+                                {                                    
+                                    if (isRun && CurrentBoard[x, y] == ' ')
+                                        result.Add(new CChessMove(CurrentBoard[i, j], i, j, x, y));
                                     else if (isRun)
                                         isRun = false;
-                                    else if (CurrentBoard[k, j] == ' ')
-                                        continue;
-                                    else 
+                                    else if (CurrentBoard[x, y] != ' ')                                        
                                     {
-                                        if (PieceLetters[1 - player].Contains(CurrentBoard[k, j]))
-                                            result.Add(new CChessMove(CurrentBoard[i, j], i, j, k, j));
-                                        break;
+                                        if (PieceLetters[1 - player].Contains(CurrentBoard[x, y]))
+                                            result.Add(new CChessMove(CurrentBoard[i, j], i, j, x, y));
+                                        return false;
                                     }
+                                    return true;
                                 }
+                               
+                                for (int k = i - 1; k >= 0; k--)
+                                    if (!CheckStraightBao(k, j))
+                                        break;
                                 isRun = true;
                                 for (int k = i + 1; k < 9; k++)
-                                {
-                                    if (isRun && CurrentBoard[k, j] == ' ')
-                                        result.Add(new CChessMove(CurrentBoard[i, j], i, j, k, j));
-                                    else if (isRun)
-                                        isRun = false;
-                                    else if (CurrentBoard[k, j] == ' ')
-                                        continue;
-                                    else
-                                    {
-                                        if (PieceLetters[1 - player].Contains(CurrentBoard[k, j]))
-                                            result.Add(new CChessMove(CurrentBoard[i, j], i, j, k, j));
+                                    if (!CheckStraightBao(k, j))
                                         break;
-                                    }
-                                }
                                 isRun = true;
                                 for (int k = j - 1; k >= 0; k--)
-                                {
-                                    if (isRun && CurrentBoard[i, k] == ' ')
-                                        result.Add(new CChessMove(CurrentBoard[i, j], i, j, i, k));
-                                    else if (isRun)
-                                        isRun = false;
-                                    else if (CurrentBoard[i, k] == ' ')
-                                        continue;
-                                    else
-                                    {
-                                        if (PieceLetters[1 - player].Contains(CurrentBoard[i, k]))
-                                            result.Add(new CChessMove(CurrentBoard[i, j], i, j, i, k));
+                                    if (!CheckStraightBao(i, k))
                                         break;
-                                    }
-                                }
                                 isRun = true;
                                 for (int k = j + 1; k < 10; k++)
-                                {
-                                    if (isRun && CurrentBoard[i, k] == ' ')
-                                        result.Add(new CChessMove(CurrentBoard[i, j], i, j, i, k));
-                                    else if (isRun)
-                                        isRun = false;
-                                    else if (CurrentBoard[i, k] == ' ')
-                                        continue;
-                                    else
-                                    {
-                                        if (PieceLetters[1 - player].Contains(CurrentBoard[i, k]))
-                                            result.Add(new CChessMove(CurrentBoard[i, j], i, j, i, k));
+                                    if (!CheckStraightBao(i, k))
                                         break;
-                                    }
-                                }
                                 break;
                             case 'G':
                             case 'g':
+                                bool CheckStraightChe(int x, int y)
+                                {
+                                    if (CurrentBoard[x, y] == ' ')
+                                        result.Add(new CChessMove(CurrentBoard[i, j], i, j, x, y));                                    
+                                    else if (CurrentBoard[x, y] != ' ')
+                                    {
+                                        if (PieceLetters[1 - player].Contains(CurrentBoard[x, y]))
+                                            result.Add(new CChessMove(CurrentBoard[i, j], i, j, x, y));
+                                        return false;
+                                    }
+                                    return true;
+                                }
+
+                                for (int k = i - 1; k >= 0; k--)
+                                    if (!CheckStraightChe(k, j))
+                                        break;                                
+                                for (int k = i + 1; k < 9; k++)
+                                    if (!CheckStraightChe(k, j))
+                                        break;                                
+                                for (int k = j - 1; k >= 0; k--)
+                                    if (!CheckStraightChe(i, k))
+                                        break;                                
+                                for (int k = j + 1; k < 10; k++)
+                                    if (!CheckStraightChe(i, k))
+                                        break;
                                 break;
                             case 'M':
                             case 'm':
+                                if (i > 0 && j > 1 && CurrentBoard[i, j - 1] == ' ' &&
+                                    !PieceLetters[player].Contains(CurrentBoard[i - 1, j - 2]))
+                                    result.Add(new CChessMove(CurrentBoard[i, j], i, j, i - 1, j - 2));
+                                if (i > 1 && j > 0 && CurrentBoard[i - 1, j] == ' ' &&
+                                    !PieceLetters[player].Contains(CurrentBoard[i - 2, j - 1]))
+                                    result.Add(new CChessMove(CurrentBoard[i, j], i, j, i - 2, j - 1));
+                                if (i > 1 && j < 9 && CurrentBoard[i - 1, j] == ' ' &&
+                                    !PieceLetters[player].Contains(CurrentBoard[i - 2, j + 1]))
+                                    result.Add(new CChessMove(CurrentBoard[i, j], i, j, i - 2, j + 1));
+                                if (i > 0 && j < 8 && CurrentBoard[i, j + 1] == ' ' &&
+                                    !PieceLetters[player].Contains(CurrentBoard[i - 1, j + 2]))
+                                    result.Add(new CChessMove(CurrentBoard[i, j], i, j, i - 1, j + 2));
+                                if (i < 9 && j < 8 && CurrentBoard[i, j + 1] == ' ' &&
+                                    !PieceLetters[player].Contains(CurrentBoard[i + 1, j + 2]))
+                                    result.Add(new CChessMove(CurrentBoard[i, j], i, j, i + 1, j + 2));
+                                if (i < 8 && j < 9 && CurrentBoard[i + 1, j] == ' ' &&
+                                    !PieceLetters[player].Contains(CurrentBoard[i + 2, j + 1]))
+                                    result.Add(new CChessMove(CurrentBoard[i, j], i, j, i + 2, j + 1));
+                                if (i < 8 && j > 0 && CurrentBoard[i + 1, j] == ' ' &&
+                                    !PieceLetters[player].Contains(CurrentBoard[i + 2, j - 1]))
+                                    result.Add(new CChessMove(CurrentBoard[i, j], i, j, i + 2, j - 1));
+                                if (i < 9 && j > 1 && CurrentBoard[i, j - 1] == ' ' &&
+                                    !PieceLetters[player].Contains(CurrentBoard[i + 1, j - 2]))
+                                    result.Add(new CChessMove(CurrentBoard[i, j], i, j, i + 1, j - 2));
                                 break;
                             case 'X':
                             case 'x':
